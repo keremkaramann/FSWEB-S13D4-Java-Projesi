1576c367ca8774ce9ee3cbe0305373b0424c90e3
Kerem Karaman <keremkaraman91@gmail.com>
2023-11-16 13:39:34 +0300

    ilk görev yapıldı

---
+++ src/com/workinthec/oop/Point.java
@@ -0,0 +1,30 @@
+package com.workinthec.oop;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+    }
+
+    public double distance(){
+        return distance(0,0);
+    }
+    public double distance(Point p){
+      return  distance(p.getX(),p.getY());
+    }
+}
